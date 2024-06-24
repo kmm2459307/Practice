e4da9c8a12f71cb2309e4fad359fbad998dd8d64
手島李来 <kmm2459307@stu.o-hara.ac.jp>
2024-06-24 11:42:36 +0900

    (EightQueen_ListCaseB)プロジェクトを登録

---
+++ TowerOfHanoi_PollName/TowerOfHanoi_PollName/TowerOfHanoi_PollName.cpp
@@ -0,0 +1,61 @@
+//=========================================================
+//	ハノイの塔　解法
+//==========================================================
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>   // printf(),scanf()
+
+typedef enum {
+	A = 1,
+	B = 2,
+	C = 3,
+} POS;
+
+// 関数プロトタイプ
+void move(int no, POS from, POS to);
+
+int main()
+{
+	while (true) {
+		int n;
+		do {
+			printf("ハノイの塔\a円盤の数:");
+			scanf("%d", &n);
+		} while (n < 1);
+
+		move(n, A, C);
+	}
+	return 0;
+}
+
+void move(int no, POS from, POS to)
+{
+	POS tmp = (POS)((A + B + C) - from - to);
+	if (no > 1) {
+		move(no - 1, from, tmp);
+	}
+	const char* strFrom = getPollName(from);
+	const char* strTo = getPollName(to);
+	printf("円盤[%d]を %s軸から %s軸へ移動\n", no, strFrom, strTo);
+
+	const char *getPollName(POS poll);
+	{
+		if (poll == A) {
+			return "A";
+		}
+		else if (poll == B) {
+			return "B";
+		}
+		else {
+			return "C";
+		}
+	}
+
+	const char *getPollName(POS poll);
+	{
+		const char* getPollName[] = { "", "A", "B", "C" };
+
+	}
+	if (no > 1) {
+		move(no - 1, tmp, to);
+	}
+}
